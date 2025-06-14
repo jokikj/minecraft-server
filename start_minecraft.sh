7aa98a038c5033bb6c7668695d9cff2fdbfcfe58
jokikj <131674744+jokikj@users.noreply.github.com>
2025-06-15 01:04:32 +0200

    Update start_minecraft.sh

--- start_minecraft.sh
+++ start_minecraft.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Variables
 if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 else
@@ -15,6 +16,8 @@ cd /home/mcadmin/minecraftserver
 screen -dmS mc java -Xmx${RAM} -Xms${RAM} -jar server.jar nogui
 EOF
 
+echo -e "\033[1;34mLe serveur Minecraft est installé!\033[0m"
+
 # Obtenir l'IP locale IPv4 uniquement
 IP_LOCALE=$(hostname -I | tr ' ' '\n' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -n1)
 
@@ -22,9 +25,15 @@ IP_LOCALE=$(hostname -I | tr ' ' '\n' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' |
 IP_PUBLIQUE=$(curl -4 -s https://api64.ipify.org)
 
 # Afficher les informations
-echo -e "\033[1;34mLe serveur Minecraft est installé et en cours d'exécution.\033[0m"
-echo -e "\033[1;34mAdresse IP locale :\033[0m \033[1;32m${IP_LOCALE}:${PORT}\033[0m"
-echo -e "\033[1;34mAdresse IP publique :\033[0m \033[1;32m${IP_PUBLIQUE}:${PORT}\033[0m"
+echo -e "\033[1;34mLancement en cours du serveur veuillez patienter...\033[0m"
+
+sleep 5
+
+CONFIG_PATH="/home/mcadmin/minecraftserver/server.properties"
+CONFIG_PORT=$(sudo -u mcadmin grep "^server-port=" "$CONFIG_PATH" | cut -d'=' -f2)
+
+echo -e "\033[1;34mAdresse IP locale :\033[0m \033[1;32m${IP_LOCALE}:${CONFIG_PORT}\033[0m"
+echo -e "\033[1;34mAdresse IP publique :\033[0m \033[1;32m${IP_PUBLIQUE}:${CONFIG_PORT}\033[0m"
 echo -e "\033[1;34mEmplacement du dossier de configuration :\033[0m \033[1;32m/home/mcadmin/minecraftserver\033[0m"
 echo -e "\033[1;34mCommandes pour gérer le serveur :\033[0m"
 echo -e "\033[1;33m  sudo -u mcadmin screen -r mc (pour rentrer dans la console du serveur minecraft)\033[0m"
