81200f92b87fd201d1f529746dd3eb7a61a9949a
jokikj <131674744+jokikj@users.noreply.github.com>
2025-06-14 11:00:32 +0200

    Update start_minecraft.sh

--- start_minecraft.sh
+++ start_minecraft.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Définir les variables
+CONFIG_DIR="/home/mcadmin/minecraftserver"
+PORT="25565"
+
 # Lancer le serveur Minecraft avec l'utilisateur mcadmin dans un screen détaché
 sudo -u mcadmin bash << EOF
 cd /home/mcadmin/minecraftserver
@@ -8,12 +12,19 @@ cd /home/mcadmin/minecraftserver
 screen -dmS mc java -Xmx1024M -Xms1024M -jar server.jar nogui
 EOF
 
-echo -e "\033[0mLe serveur Minecraft a été lancé avec succès.\033[0m"
-echo -e "\033[1;34m----------------------------------------\033[0m"
-echo -e "Adresse IP Privé : \033[1;32m$(hostname -I | awk '{print $1}'):25565\033[0m"
-echo -e "Commandes pour gérer le serveur :"
-echo -e "- Pour accéder au fichier de configuration : \033[1;32m/home/mcadmin/minecraftserver/server.properties\033[0m"
-echo -e "- Pour accéder à la console du serveur : \033[1;32msudo -u mcadmin screen -r mc\033[0m"
-echo -e "- Pour détacher la console : \033[1;32mCtrl + A, D\033[0m"
-echo -e "- Pour arrêter le serveur : \033[1;32m/stop\033[0m"
-echo -e "- Pour quitter la console : \033[1;32mCtrl + D\033[0m"
+# Obtenir l'IP locale IPv4 uniquement
+IP_LOCALE=$(hostname -I | tr ' ' '\n' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -n1)
+
+# Obtenir l'IP publique IPv4 uniquement
+IP_PUBLIQUE=$(curl -4 -s https://api64.ipify.org)
+
+# Afficher les informations
+echo -e "\033[1;34mLe serveur Minecraft est installé et en cours d'exécution.\033[0m"
+echo -e "\033[1;34mAdresse IP locale :\033[0m \033[1;32m${IP_LOCALE}:${PORT}\033[0m"
+echo -e "\033[1;34mAdresse IP publique :\033[0m \033[1;32m${IP_PUBLIQUE}:${PORT}\033[0m"
+echo -e "\033[1;34mEmplacement du dossier de configuration :\033[0m \033[1;32m${CONFIG_DIR}\033[0m"
+echo -e "\033[1;34mCommandes pour gérer le serveur :\033[0m"
+echo -e "\033[1;33m  sudo -u mcadmin screen -r mc\033[0m"
+echo -e "\033[1;33m  Ctrl + A, D (pour détacher la console)\033[0m"
+echo -e "\033[1;33m  stop (pour arrêter le serveur)\033[0m"
+echo -e "\033[1;33m  Ctrl + D (pour quitter la console)\033[0m"
