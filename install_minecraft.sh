e02fe2d870855215ae1fa07e1f2b2336dfafe63e
jokikj <131674744+jokikj@users.noreply.github.com>
2025-06-14 14:21:11 +0200

    Update install_minecraft.sh

--- install_minecraft.sh
+++ install_minecraft.sh
@@ -43,6 +43,73 @@ wget https://piston-data.mojang.com/v1/objects/e6ec2f64e6080b9b5d9b471b291c33cc7
 # Accepter l'EULA
 echo -e "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://aka.ms/MinecraftEULA).\n#$(date -u)\neula=true" > eula.txt
 
+# Créer server.properties
+sudo -u mcadmin bash -c "cat <<EOL > /home/mcadmin/minecraftserver/server.properties
+#Minecraft server properties
+#$(date -u)
+accepts-transfers=false
+allow-flight=false
+allow-nether=true
+broadcast-console-to-ops=true
+broadcast-rcon-to-ops=true
+bug-report-link=
+difficulty=easy
+enable-command-block=false
+enable-jmx-monitoring=false
+enable-query=false
+enable-rcon=false
+enable-status=true
+enforce-secure-profile=true
+enforce-whitelist=false
+entity-broadcast-range-percentage=100
+force-gamemode=false
+function-permission-level=2
+gamemode=survival
+generate-structures=true
+generator-settings={}
+hardcore=false
+hide-online-players=false
+initial-disabled-packs=
+initial-enabled-packs=vanilla
+level-name=world
+level-seed=
+level-type=minecraft:normal
+log-ips=true
+max-chained-neighbor-updates=1000000
+max-players=20
+max-tick-time=60000
+max-world-size=29999984
+motd=A Minecraft Server
+network-compression-threshold=256
+online-mode=true
+op-permission-level=4
+pause-when-empty-seconds=60
+player-idle-timeout=0
+prevent-proxy-connections=false
+pvp=true
+query.port=${PORT}
+rate-limit=0
+rcon.password=
+rcon.port=$((PORT + 10))
+region-file-compression=deflate
+require-resource-pack=false
+resource-pack=
+resource-pack-id=
+resource-pack-prompt=
+resource-pack-sha1=
+server-ip=
+server-port=${PORT}
+simulation-distance=10
+spawn-monsters=true
+spawn-protection=16
+sync-chunk-writes=true
+text-filtering-config=
+text-filtering-version=0
+use-native-transport=true
+view-distance=10
+white-list=false
+EOL"
+
 # Lancer le serveur dans un screen détaché
 screen -dmS mc java -Xmx${RAM} -Xms${RAM} -jar server.jar nogui
 EOF
