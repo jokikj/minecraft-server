349dc961c518e8ddd2dc4877a95aeae204d9c857
jokikj <131674744+jokikj@users.noreply.github.com>
2025-06-15 10:27:27 +0200

    Update start_minecraft.sh

--- start_minecraft.sh
+++ start_minecraft.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Variable dans le fichier .env
+# Variables from the .env file
 if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 else
@@ -8,53 +8,51 @@ else
     exit 1
 fi
 
-# Chemin vers server.properties
+# Path to server.properties
 SERVER_PROPERTIES="/home/${USER}/minecraftserver/server.properties"
 
-# Lire le port actuel dans le fichier
+# Read the current port from the config file
 CONFIG_PORT=$(sudo -u ${USER} grep "^server-port=" "$SERVER_PROPERTIES" | cut -d'=' -f2)
 
 if [[ "$CONFIG_PORT" != "$PORT" ]]; then
-  echo -e "\033[1;33mLe port actuel est $CONFIG_PORT, changement du port en cours...\033[0m"
+  echo -e "\033[1;33mCurrent port is $CONFIG_PORT, updating port...\033[0m"
 
-  # Modifier le port dans le fichier de config
+  # Update the port in the config file
   sudo -u ${USER} sed -i "s/^server-port=.*/server-port=${PORT}/" "$SERVER_PROPERTIES"
-  echo -e "\033[1;32mPort changé avec succès dans $SERVER_PROPERTIES : server-port=${PORT}\033[0m"
+  echo -e "\033[1;32mPort successfully changed in $SERVER_PROPERTIES: server-port=${PORT}\033[0m"
 
-  # Lancer le serveur Minecraft avec l'utilisateur mcadmin dans un screen détaché
+  # Start the Minecraft server as the user in a detached screen session
   sudo -u ${USER} bash << EOF
 cd /home/${USER}/minecraftserver
-# Lancer dans un screen nommé 'mc' pour lancer le serveur Minecraft
+# Start in a screen session named 'mc' to run the Minecraft server
 screen -dmS mc java -Xmx${RAM} -Xms${RAM} -jar server.jar nogui
 EOF
 else
-  # Lancer le serveur Minecraft avec l'utilisateur mcadmin dans un screen détaché
+  # Start the Minecraft server as the user in a detached screen session
   sudo -u ${USER} bash << EOF
 cd /home/${USER}/minecraftserver
-# Lancer dans un screen nommé 'mc' pour lancer le serveur Minecraft
+# Start in a screen session named 'mc' to run the Minecraft server
 screen -dmS mc java -Xmx${RAM} -Xms${RAM} -jar server.jar nogui
 EOF
-  # Afficher les informations
-  echo -e "\033[1;34mLancement en cours du serveur veuillez patienter...\033[0m"
+  # Display information
+  echo -e "\033[1;34mServer is starting, please wait...\033[0m"
   sleep 5
 fi
 
-# Obtenir l'IP locale IPv4 uniquement
+# Get local IPv4 address only
 IP_LOCALE=$(hostname -I | tr ' ' '\n' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -n1)
-# Obtenir l'IP publique IPv4 uniquement
+# Get public IPv4 address only
 IP_PUBLIQUE=$(curl -4 -s https://api64.ipify.org)
-# Obtenir le port dans le fichier de configuration
+# Get the port from the config file
 CONFIG_PORT=$(sudo -u ${USER} grep "^server-port=" "$SERVER_PROPERTIES" | cut -d'=' -f2)
 
-# Afficher les informations
+# Display server information
 echo -e "\033[1;34m-------------------------------\033[0m"
-echo -e "\033[1;34mInformation sur le serveur :\033[0m"
-echo -e "\033[1;34mAdresse IP locale :\033[0m \033[1;32m${IP_LOCALE}:${CONFIG_PORT}\033[0m"
-echo -e "\033[1;34mAdresse IP publique :\033[0m \033[1;32m${IP_PUBLIQUE}:${CONFIG_PORT}\033[0m \033[1;36m(Si le port est ouvert sur la box)\033[0m"
-echo -e "\033[1;34mEmplacement du dossier de configuration :\033[0m \033[1;32m/home/${USER}/minecraftserver\033[0m"
-echo -e "\033[1;34mCommandes pour gérer le serveur :\033[0m"
-echo -e "\033[1;33m  sudo -u ${USER} screen -r mc \033[1;36m(pour rentrer dans la console du serveur minecraft)\033[0m"
-echo -e "\033[1;33m  /stop \033[1;36m(pour arrêter le serveur)\033[0m"
-echo -e "\033[1;33m  Ctrl + A, D \033[1;36m(pour détacher la console)\033[0m"
-echo -e "\033[1;33m  Ctrl + D \033[1;36m(pour quitter la console)\033[0m"
-
+echo -e "\033[1;34mServer information:\033[0m"
+echo -e "\033[1;34mLocal IP address:\033[0m \033[1;32m${IP_LOCALE}:${CONFIG_PORT}\033[0m"
+echo -e "\033[1;34mPublic IP address:\033[0m \033[1;32m${IP_PUBLIQUE}:${CONFIG_PORT}\033[0m \033[1;36m(if the port is open on your router)\033[0m"
+echo -e "\033[1;34mServer folder location:\033[0m \033[1;32m/home/${USER}/minecraftserver\033[0m"
+echo -e "\033[1;34mCommands to manage the server:\033[0m"
+echo -e "\033[1;33m  sudo -u ${USER} screen -r mc \033[1;36m(to access the Minecraft server console)\033[0m"
+echo -e "\033[1;33m  /stop \033[1;36m(to stop the server)\033[0m"
+echo -e "\033[1;33m  Ctrl + A, D \033[1;36m(to detach the console)\033[0m"
