5db4a4d1fa901106416fb9c78b0fb5be8144bc4b
jokikj <131674744+jokikj@users.noreply.github.com>
2025-06-15 02:05:56 +0200

    Update install_minecraft.sh

--- install_minecraft.sh
+++ install_minecraft.sh
@@ -21,23 +21,23 @@ sudo iptables -I INPUT -p tcp --dport ${PORT} -j ACCEPT
 
 # Créer utilisateur
 
-# Vérifier si l'utilisateur mcadmin existe déjà
+# Vérifier si l'utilisateur existe déjà
 
-if id "mcadmin" &>/dev/null; then
-  echo "L'utilisateur mcadmin existe déjà."
+if id "${USER}" &>/dev/null; then
+  echo "\033[1;34mL'utilisateur ${USER} existe déjà.\033[0m"
 else
-  sudo adduser --gecos "" --disabled-password mcadmin
-  echo "mcadmin:mcadmin" | sudo chpasswd
-  echo "Utilisateur mcadmin créé avec succès."
+  sudo adduser --gecos "" --disabled-password ${USER}
+  echo "${USER}:${USER}" | sudo chpasswd
+  echo "\033[1;34mUtilisateur ${USER} créé avec succès.\033[0m"
 fi
 
 # Créer le dossier du serveur Minecraft
-sudo mkdir -p /home/mcadmin/minecraftserver
-sudo chown mcadmin:mcadmin /home/mcadmin/minecraftserver
+sudo mkdir -p /home/${USER}/minecraftserver
+sudo chown ${USER}:${USER} /home/${USER}/minecraftserver
 
-# Télécharger le serveur Minecraft avec l'utilisateur mcadmin
-sudo -u mcadmin bash << EOF
-cd /home/mcadmin/minecraftserver
+# Télécharger le serveur Minecraft avec l'utilisateur
+sudo -u ${USER} bash << EOF
+cd /home/${USER}/minecraftserver
 wget https://piston-data.mojang.com/v1/objects/e6ec2f64e6080b9b5d9b471b291c33cc7f509733/server.jar
 
 # Accepter l'EULA
@@ -60,14 +60,14 @@ echo -e "\033[1;34mLancement en cours du serveur veuillez patienter...\033[0m"
 
 sleep 20
 
-CONFIG_PATH="/home/mcadmin/minecraftserver/server.properties"
-CONFIG_PORT=$(sudo -u mcadmin grep "^server-port=" "$CONFIG_PATH" | cut -d'=' -f2)
+CONFIG_PATH="/home/${USER}/minecraftserver/server.properties"
+CONFIG_PORT=$(sudo -u ${USER} grep "^server-port=" "$CONFIG_PATH" | cut -d'=' -f2)
 
 echo -e "\033[1;34mAdresse IP locale :\033[0m \033[1;32m${IP_LOCALE}:${CONFIG_PORT}\033[0m"
 echo -e "\033[1;34mAdresse IP publique :\033[0m \033[1;32m${IP_PUBLIQUE}:${CONFIG_PORT}\033[0m"
-echo -e "\033[1;34mEmplacement du dossier de configuration :\033[0m \033[1;32m/home/mcadmin/minecraftserver\033[0m"
+echo -e "\033[1;34mEmplacement du dossier de configuration :\033[0m \033[1;32m/home/${USER}/minecraftserver\033[0m"
 echo -e "\033[1;34mCommandes pour gérer le serveur :\033[0m"
-echo -e "\033[1;33m  sudo -u mcadmin screen -r mc (pour rentrer dans la console du serveur minecraft)\033[0m"
+echo -e "\033[1;33m  sudo -u ${USER} screen -r mc (pour rentrer dans la console du serveur minecraft)\033[0m"
 echo -e "\033[1;33m  Ctrl + A, D (pour détacher la console)\033[0m"
 echo -e "\033[1;33m  /stop (pour arrêter le serveur)\033[0m"
 echo -e "\033[1;33m  Ctrl + D (pour quitter la console)\033[0m"
